5de317073b4e8eefd04e4c1d2ea2059ea0ee3cea
omohayui <omohayui@gmail.com>
2019-10-03 10:02:22 +0900

    remove gvimrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/_vimrc ~/.vimrc
-ln -sf ~/dotfiles/_gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/_zshrc ~/.zshrc
 ln -sf ~/dotfiles/_gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/_config/karabiner ~/.config/karabiner
