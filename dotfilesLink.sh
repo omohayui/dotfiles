466cad0e2bc557ef39c0ec50aa45c99042df0c3d
omohayui <omohayui@gmail.com>
2019-10-13 17:41:16 +1300

    add ideavimrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/_vimrc ~/.vimrc
 ln -sf ~/dotfiles/_zshrc ~/.zshrc
 ln -sf ~/dotfiles/_gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/_config/karabiner ~/.config/karabiner
+ln -sf ~/dotfiles/_ideavimrc ~/.ideavimrc
